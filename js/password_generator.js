d98612e297da86003717ff64f947bae8d4efde97
Udara ranasinghe <indi79@hotmail.com>
2020-03-05 11:19:42 +1030

    adding verifications to Character Types , Special Characters, Password Length

--- js/password_generator.js
+++ js/password_generator.js
@@ -1,72 +1,183 @@
+var errors_div =  document.querySelector("#errors");
+
+function generateRandomLetter(random_number) {
+
+    let alphabet = 'pcsdetrbfgyoxhikuljvmnqza';
+    return alphabet[random_number];
+
+}
+
+function generateRandomDigit(random_number) {
+
+    let  digits = '0816394527';
+    return digits[random_number];
+
+}
+
+function generateRandomSpecialCharacter(random_number) {
+
+    let  special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; // 33 in total
+    return special_characters[random_number];
+
+}
+
 function generateCharacter(character_type) {
+
     switch(character_type) {
-        case 'uppercase': return String.fromCharCode ( Math.floor ( (Math.random() * ( 90 - 65 ) + 65 ) ) ); // ASCII range for Uppercase
+        case 'uppercase': return generateRandomLetter( generateRandomNnumber(25) ).toUpperCase();
           break;
-        case 'lowercase': return String.fromCharCode ( Math.floor ( Math.random() * ( 122 - 97 ) + 97 ) ) ; // ASCII range for Lowercase
+        case 'lowercase': return generateRandomLetter( generateRandomNnumber(25) );
           break;
-        case 'number': return String.fromCharCode ( Math.floor ( Math.random() * (58 - 48) + 48 ) ); // ASCII range for Digits
+        case 'number': return  generateRandomDigit( generateRandomNnumber(9) );
             break;
-        case 'special_character': return String.fromCharCode(Math.floor( Math.random() * ( 64 - 32 ) + 32 ) ); // ASCII range for Special Characters
+        case 'special_character': return generateRandomSpecialCharacter( generateRandomNnumber(32) );
             break;
         default:
       }
+
 }
 
-function generatePassword(expected_password_length,final_combination_of_character_types) {
+function generateRandomNnumber( upper_limit ) {
+
+    return Math.floor( Math.random() * upper_limit );
+
+}
+
+function randomlyChooseCharacterType( character_type_array ) {
+
+    let number_of_elements_in_array = character_type_array.length;
+    return character_type_array [ generateRandomNnumber( number_of_elements_in_array ) ];
+
+}
+
+function generatePassword( expected_password_length , final_combination_of_character_types ) {
+
     let password = '';
 
-    for (let i = 0; i < expected_password_length; i++) {
-        //generate random character type from final_combination_of_character_types Array
-        let random_character_type = final_combination_of_character_types[ Math.floor(Math.random() * final_combination_of_character_types.length) ];
-        let generated_character = generateCharacter(random_character_type);
+    for ( let i = 0; i < expected_password_length; i++ ) {
+        let character_type = randomlyChooseCharacterType( final_combination_of_character_types );
+        let generated_character = generateCharacter( character_type );
         password += generated_character;
     }
     return password;
+
 }
 
-function includeSpecialCharacters(chosen_character_types,include_special_characters) {
-    if(include_special_characters == 'true') { 
-        chosen_character_types.push("special_character");}
+// If user wants to include special characters then push "special_character" to chosen_character_types Array
+function includeSpecialCharacters( chosen_character_types , include_special_characters ) {
+
+    if( include_special_characters == 'true' ) { 
+        chosen_character_types.push ( "special_character" ); 
+    }
+
     return chosen_character_types;
 }
 
+// Get password length from UI
+// Update current password lenth value real time on UI
+// Validate password length (between 8 - 128 characters)
+// Throw an error if not in acceptable range
 function getPasswordLength() {
-    let password_length = document.getElementById("password-length").value;
-    if (password_length >= 8 && password_length <= 128){
+
+    let password_length = parseInt( document.querySelector("#password-length").value);
+    let password_length_display = document.querySelector('#password-length-display');
+
+    password_length_display.innerHTML = password_length;
+
+    if ((password_length >= 8 && password_length <= 128) && Number.isInteger( password_length ) ){
         return password_length;
     }
     else{
-        alert("Password length must be between 8 - 128 characters");
+        errors_div.innerHTML += '<li>Password length must be between 8 - 128 characters</li>';
     }
 }
 
+// Check input is acceptable then returns a boolean 
+function inputIsAccaptable(inputs,check_against)
+{
+      return inputs.every(function (value) {
+        return (check_against.indexOf(value) >= 0);
+      });
+}
+
+// Get character_type[] from UI
+// if array has elements : check if input is acceptable : if false throw an error else 
+
 function getCharacterTypes() {
+
     let character_type = [];
     character_type = document.querySelectorAll('input[name="character-type[]"]:checked');
     let character_type_array = [];
+
     for (let i = 0; i < character_type.length; i++) {
-        character_type_array.push ( character_type[i].value );
+        character_type_array.push( character_type[i].value );
     }
-    return character_type_array;
+
+    if(character_type_array.length > 0){
+        let acceptable_answers = ['uppercase','lowercase','number'];
+        if(inputIsAccaptable(character_type_array,acceptable_answers))
+        {
+        return character_type_array;
+        }
+        else{
+            errors_div.innerHTML += '<li>Character Types : One or more options are invalid </li>';
+            return character_type_array = [];
+        }
+    }
+    else{
+        errors_div.innerHTML += '<li>Please select character type</li>';
+        return character_type_array;
+    }
+
 }
 
 function haveSpecialCharacters() {
+
     let special_characters = [];
     special_characters = document.querySelectorAll('input[name="specialcharacters[]"]:checked');
     let special_characters_array = [];
+
     for (let i = 0; i < special_characters.length; i++) {
-        special_characters_array.push ( special_characters[i].value );
+        special_characters_array.push( special_characters[i].value );
     }
-    return special_characters_array[0];
+    
+    if(special_characters_array.length > 0) {
+        let acceptable_answers = ['true','false'];
+        if(inputIsAccaptable(special_characters_array,acceptable_answers))
+        {
+        return special_characters_array[0];
+        }
+        else{
+            errors_div.innerHTML += '<li>Special Characters : Input is invalid </li>';
+            return special_characters_array = [];
+        }
+    }   
+    else {
+        errors_div.innerHTML += '<li>Please select special characters </li>';
+        return special_characters_array;
+    }
+    
 }
 
-document.getElementById('password-length').oninput = function() {
-    let passwordLengthDisplay = document.getElementById('password-length-display');
-    let passwordLength = document.getElementById('password-length');
-    passwordLengthDisplay.innerHTML = passwordLength.value;
-};
+/* Event listener for password_length_range_slider on input and on load */
+var password_length_range_slider = document.querySelector('#password-length');
 
-document.getElementById("generate-password").addEventListener("click", function () {
+password_length_range_slider.addEventListener('input', function() {
+    getPasswordLength();
+});
+
+window.addEventListener('load', function() {
+    getPasswordLength();
+});
+
+/* Event listener for btn_generate_password */
+var btn_generate_password = document.querySelector('#generate-password');
+
+btn_generate_password.addEventListener("click", function () {
+
+    errors_div.innerHTML = '';
+
+    let password_text = document.querySelector('#password'); 
     let expected_password_length = getPasswordLength();
     let chosen_character_types = getCharacterTypes();
     let have_special_characters = haveSpecialCharacters();
@@ -75,9 +186,7 @@ document.getElementById("generate-password").addEventListener("click", function 
         // if have_special_characters == 'true' : add special_character to chosen_character_types Array
         let final_combination_of_character_types = includeSpecialCharacters(chosen_character_types,have_special_characters);
 
-       document.getElementById('password').value =  generatePassword(expected_password_length,final_combination_of_character_types);
+        password_text.value =  generatePassword(expected_password_length,final_combination_of_character_types);
     }
-    else {
-        alert("Please select at least 1 option from Character Type & Special Characters");
-    }
+
 });
